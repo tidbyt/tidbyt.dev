679f5f8bd332cb97858e07e3c5ccce26ac8ec79b
Mark Spicer <mark@tidbyt.com>
2022-09-22 12:59:07 -0400

    core: Add redirects and 404 page. (#69)
    This commit adds redirects so that each subdirectory redirects to a page
    and adds a 404 page so dead links go somewhere.

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes as RouterRoutes } from "react-router-dom";
+import { BrowserRouter, Route, Routes as RouterRoutes, Navigate } from "react-router-dom";
 
 import Main from '../main/Main';
 import Home from '../home/Home';
 import { getDocs } from './docs';
 import PublicAPI from '../publicapi/PublicAPI';
 import ScrollToTop from './ScrollToTop';
+import NotFound from '../notfound/NotFound';
 
 
 export default function Routes() {
@@ -27,6 +28,13 @@ export default function Routes() {
                 <Route path="/" element={<Home />} />
                 <Route path="/docs/api" element={<Main ><PublicAPI /></Main>} />
                 {genRoutes()}
+                <Route path='/docs' element={<Navigate to='/docs/overview/getting-started' />} />
+                <Route path='/docs/overview' element={<Navigate to='/docs/overview/getting-started' />} />
+                <Route path='/docs/build' element={<Navigate to='/docs/build/build-for-tidbyt' />} />
+                <Route path='/docs/integrate' element={<Navigate to='/docs/integrate/pushing-apps' />} />
+                <Route path='/docs/publish' element={<Navigate to='/docs/publish/community-apps' />} />
+                <Route path='/docs/engage' element={<Navigate to='/docs/engage/community' />} />
+                <Route path='*' element={<Main ><NotFound /></Main>} />
             </RouterRoutes>
         </BrowserRouter>
     )
